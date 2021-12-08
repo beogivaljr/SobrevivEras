3ff52597bc3240391d0159ce5dfed75540d7dcaa
Beogival jr <beogivaljr@gmail.com>
2021-12-08 18:34:29 -0300

    fix to signal call

--- scenes/opponentPanel.gd
+++ scenes/opponentPanel.gd
@@ -5,14 +5,14 @@ var first_enemy = null
 func _ready():
 	if $marginContainer/opponentPanel/twnPosition.is_active():
 		$colorRect.modulate.a=0
-	darkenBg()
+	darkenBg(null, null)
 	var _s1 = $marginContainer/opponentPanel/btnExit/twnPosition.connect("tween_started",self,'lightenBg')
 	self.first_enemy = $marginContainer/opponentPanel.first_enemy
 
 
-func darkenBg():
+func darkenBg(_a, _b):
 	$twn.interpolate_property($colorRect,'modulate:a',$colorRect.modulate.a,0.66,1.0,Tween.TRANS_QUINT,Tween.EASE_IN)
 	$twn.start()
-func lightenBg():
+func lightenBg(_a, _b):
 	$twn.interpolate_property($colorRect,'modulate:a',$colorRect.modulate.a,0,0.33,Tween.TRANS_QUINT,Tween.EASE_IN)
 	$twn.start()
